7966d4de057c08c45909a411c69940309b631f62
Paivi Suomela <paivi.e.suomela@gmail.com>
2015-07-04 15:29:31 +0100

    added data validation to amount, terms and VAT textboxes

--- TermsCalculator/TermsCalculator.xaml.cs
+++ TermsCalculator/TermsCalculator.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -12,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Globalization;
 
 namespace TermsCalculator
 {
@@ -27,8 +29,10 @@ namespace TermsCalculator
 
         private void calculateButton_Click(object sender, RoutedEventArgs e)
         {
-            MessageBox.Show("Hello.");
+
+            //MessageBox.Show("Hello.");
         }
+
 
         private void discountRadioButton_Checked(object sender, RoutedEventArgs e)
         {
@@ -37,13 +41,13 @@ namespace TermsCalculator
 
         private void scRadioButton_Click(object sender, RoutedEventArgs e)
         {
-           // if (sender == null) return;
             servOrDiscTextBlock.Text = "Service Charge %";
             
         }
 
         private void calculateVATCheckBox_Click(object sender, RoutedEventArgs e)
         {
+            // enable all the VAT related fields if checkbox is checked
             if (calculateVATCheckBox.IsChecked == true)
             {
                 vatPercentage.IsEnabled = true;
@@ -61,5 +65,52 @@ namespace TermsCalculator
                 vatPercentageTextBlock.Foreground = Brushes.Gray;
             }
         }
+
+
+        private void amountBox_LostFocus(object sender, RoutedEventArgs e)
+        {
+            double result;
+            if (!Double.TryParse(amountBox.Text, out result)) {
+                MessageBox.Show("Please enter number as amount!");
+            }
+        }
+
+        private void vatPercentage_LostFocus(object sender, RoutedEventArgs e)
+        {
+
+            checkPercentage(vatPercentage.Text);
+
+        }
+
+        private void checkPercentage(string entered_text)
+        {
+            double result;
+            if (!Double.TryParse(entered_text, out result))
+            {
+                MessageBox.Show("Please enter a percentage between 0 and 100!");
+                return;
+
+            }
+
+            double enteredAmount = Double.Parse(entered_text, NumberStyles.AllowDecimalPoint);
+
+            if (enteredAmount <= 0)
+            {
+                MessageBox.Show("The percentage must be more than 0!");
+            }
+            else if (enteredAmount > 100.0)
+            {
+                MessageBox.Show("The percentage must be less than 100!");
+            }
+        }
+
+        private void termsPercentage_LostFocus(object sender, RoutedEventArgs e)
+        {
+            checkPercentage(termsPercentage.Text);
+        }
+
+   
+
+
     }
 }
